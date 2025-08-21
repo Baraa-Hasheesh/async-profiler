522f364ae030741cf38cd28b45dbe80719c025e2
Bara' Hasheesh <bara.hasheesh@gmail.com>
2025-08-21 14:35:15 +0100

    remove interval

--- test/test/jfr/JfrTests.java
+++ test/test/jfr/JfrTests.java
@@ -69,7 +69,7 @@ public class JfrTests {
      * @param p The test process to profile with.
      * @throws Exception Any exception thrown during profiling JFR output parsing.
      */
-    @Test(mainClass = JfrMultiModeProfiling.class, agentArgs = "start,threads,event=cpu,interval=1us,alloc,lock=0,quiet,cstack=vmx,jfr,file=%f",
+    @Test(mainClass = JfrMultiModeProfiling.class, agentArgs = "start,threads,event=cpu,alloc,lock=0,quiet,cstack=vmx,jfr,file=%f",
             output = true, jvmVer = {8, 15}, jvmArgs = "-XX:-UseBiasedLocking")
     public void parseMultiModeRecording(TestProcess p) throws Exception {
         Output output = p.waitForExit(TestProcess.STDOUT);
