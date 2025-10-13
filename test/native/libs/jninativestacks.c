465282d9f9a509449e9dba355d9ef54f0c7c2915
Bara' Hasheesh <bara.hasheesh@gmail.com>
2025-10-13 12:15:18 +0100

    debug

--- test/native/libs/jninativestacks.c
+++ test/native/libs/jninativestacks.c
@@ -5,18 +5,12 @@
 
 #include <jni.h>
 #include <math.h>
-
-JNIEXPORT double largeInnerFrameFinal(int i) {
-    char frame[0x10000];
-    // Prevent from being optimized by compiler
-    (void)frame;
-
-    return sqrt(i) + pow(i, sqrt(i));
-}
-
-JNIEXPORT double largeInnerFrameIntermediate(int i) {
-    return largeInnerFrameFinal(i) + largeInnerFrameFinal(i + 1);
-}
+#include <dlfcn.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "asprof.h"
 
 JNIEXPORT double doCpuTask() {
     int i = 0;
@@ -60,15 +54,18 @@ JNIEXPORT jdouble JNICALL Java_test_stackwalker_StackGenerator_leafFrame(JNIEnv*
     return doCpuTask();
 }
 
-JNIEXPORT jdouble JNICALL Java_test_stackwalker_StackGenerator_largeInnerFrame(JNIEnv* env, jclass cls) {
-    double result = 0;
-    for (int i = 0; i < 100000000; i++) {
-        result += largeInnerFrameIntermediate(i);
-    }
-    return result;
-}
+#include <stdio.h>
 
 int main() {
-    doCpuTask();
+    void* lib = dlopen("build/lib/libasyncProfiler.dylib", RTLD_NOW);
+    asprof_init_t asprof_init = dlsym(lib, "asprof_init");
+    asprof_init();
+
+    asprof_execute_t asprof_execute = dlsym(lib, "asprof_execute");
+    asprof_error_t err = asprof_execute("start,event=cpu,collapsed,file=output", NULL);
+
+    fprintf(stderr, "%.02f\n", doCpuTask());
+
+    err = asprof_execute("stop", NULL);
     return 0;
 }
